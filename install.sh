baf902e28b365fe667316bf4bf84f2d6c4f0d469
sudhakar-pujair <54796582+sudhakar-pujair@users.noreply.github.com>
2019-10-17 20:34:44 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-sudo apt update
-sudo apt install -y tree
 rm -rf /tmp/folder
 mkdir /tmp/folder
 echo echo $(date)>>/tmp/folder/file1
@@ -12,4 +10,10 @@ sleep 2s
 echo echo $(date)>>/tmp/folder/file1
 sleep 2s
 echo echo $(date)>>/tmp/folder/file1
+sleep 2s
+echo echo $(date)>>/tmp/folder/file2
+sleep 2s
+echo echo $(date)>>/tmp/folder/file3
+sleep 2s
+echo echo $(date)>>/tmp/folder/file4
 sleep 2s
